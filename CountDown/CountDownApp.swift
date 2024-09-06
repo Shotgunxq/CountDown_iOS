a15224a16cf8e95c169c57a02f9bf0a46d1272a1
Bence <bence.bodnar1@ibm.com>
2024-09-06 16:24:39 +0200

    Initial Commit

---
+++ CountDown/CountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownApp.swift
+//  CountDown
+//
+//  Created by Bence Bodnár on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
